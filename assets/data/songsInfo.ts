4791913487b066dfe95a23e3c75c8903fd8296d9
sing6831 <52940625+sing6831@users.noreply.github.com>
2019-07-29 15:10:27 -0400

    Add files via upload

---
+++ assets/data/songsInfo.ts
@@ -0,0 +1,40 @@
+import{songInfo}from '../../app/songInfo';
+
+export const SongsDetails :
+
+songInfo[] =[{
+    songName:'Senorita',
+    songArtist: 'Shawn Mendes, Camila Cabello',
+    songGenre:'pop',
+    songYear:'2018',
+    songPicture:'assets/images/one.jpeg',
+},
+{
+    songName:'Kiki',
+    songArtist: 'Drake',
+    songGenre:'Hip-Hop/Rap',
+    songYear:'2018',
+    songPicture:'assets/images/two.jpg',
+
+},
+{
+    songName:'Sorry',
+    songArtist: 'Justin Beiber',
+    songGenre:'Pop',
+    songYear:'2015',
+    songPicture:'assets/images/three.png',
+
+},
+{
+    songName:'Waka Waka',
+    songArtist: 'Shakira',
+    songGenre:'Pop',
+    songYear:'2010',
+    songPicture:'assets/images/four.jpg',
+
+},
+
+
+
+]
+
